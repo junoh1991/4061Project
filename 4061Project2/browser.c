925066418e413a39f5bf20fc2285cc1c59fb1ae9
junoh1991 <ohxxx371@csel-kh1250-36.cselabs.umn.edu>
2017-03-11 17:31:30 -0600

    Can open new tabs. Will pipe more than just controller-router

--- 4061Project2/browser.c
+++ 4061Project2/browser.c
@@ -76,6 +76,40 @@ void create_new_tab_cb(GtkButton *button, gpointer data)
     child_req_to_parent new_tab = {CREATE_TAB, b};
     // Send the struct to ROUTER
     write(channel.child_to_parent_fd[1], &new_tab, sizeof(&new_tab));
+	show_browser();
+}
+
+/*
+ * Name:                router_create_tab
+ * Input arguments:     'channel': Includes pipes which new url process will use to 
+ *						communicate with Router process
+ *						'tab_index: tab index of the new url process
+ * Output arguments:    none
+ * Function:            This function will make a new url_rendering_process
+ */
+void router_create_tab(comm_channel *channel, int tab_index)
+{
+	int flags;
+	int new_tab_pid = fork();
+	if (new_tab_pid > 0)  // Parent
+	{	
+		close(channel -> child_to_parent_fd[1]);
+		close(channel -> parent_to_child_fd[0]);
+		flags = fcntl(channel -> child_to_parent_fd[0], F_GETFL, 0);
+		fcntl(channel -> child_to_parent_fd[0], F_SETFL, flags | O_NONBLOCK);
+	}
+	else if (new_tab_pid == 0) // Child
+	{
+		close(channel -> child_to_parent_fd[0]);
+		close(channel -> parent_to_child_fd[1]);
+		flags = fcntl(channel -> child_to_parent_fd[0], F_GETFL, 0);
+		fcntl(channel-> child_to_parent_fd[0], F_SETFL, flags | O_NONBLOCK);
+		url_rendering_process(tab_index, channel);
+	}
+	else
+	{
+		perror("Fork Error");
+	}
 }
 
 /*
@@ -94,11 +128,15 @@ int url_rendering_process(int tab_index, comm_channel *channel) {
 	// Create url-rendering window
 	create_browser(URL_RENDERING_TAB, tab_index, NULL, NULL, &b_window, channel);
 	child_req_to_parent req;
+	int answer;
+	char received_url[100];
 	while (1) {
+		// if (read(channel->parent_to_child_fd[0], received_url, sizeof(received_url)) == -1)
+			process_single_gtk_event();
+		// else
+		//	render_web_page_in_tab(received_url, b_window);
+		// TAB_KILLED received
 		usleep(1000);
-		// Append your code here
-		// Handle 'which' messages?
-		//
 	}
 	return 0;
 }
@@ -121,6 +159,11 @@ int controller_process(comm_channel *channel) {
 	return 0;
 }
 
+
+
+
+
+
 /*
  * Name:                router_process
  * Input arguments:     none
@@ -130,73 +173,81 @@ int controller_process(comm_channel *channel) {
 int router_process() {
 	comm_channel *channel[MAX_TAB];
 	channel[0] = (comm_channel *) malloc(sizeof(comm_channel));
+	
 	int tab_index = 1;
-	int channel_index = 1;
-    
-    int router_controller[2];
-    int router_url[MAX_TAB][2];
-    int pid, flags;
-    int proceIndex = 0;
-    int read_error;
+    int pid, flags, command_type, i;
+	
+	// buffer to store message
     child_req_to_parent *temp = (child_req_to_parent*)malloc(sizeof(child_req_to_parent));   
  
     // Create a pipe b/w router and controller
-    if (pipe(router_controller) == -1)
+    if (pipe((channel[0] -> child_to_parent_fd)) == -1)
     {
         perror("pipe error");
         exit(1);
     }
     // Set Non block-read from controller.
-    flags = fcntl(router_controller[0], F_GETFL, 0);
-    fcntl(router_controller[0], F_SETFL, flags | O_NONBLOCK);
-    channel[0] -> child_to_parent_fd[0] = router_controller[0];    
-    channel[0] -> child_to_parent_fd[1] = router_controller[1];    
+    flags = fcntl(channel[0] -> child_to_parent_fd[0], F_GETFL, 0);
+    fcntl(channel[0] -> child_to_parent_fd[0], F_SETFL, flags | O_NONBLOCK);
     pid = fork();
+	
     // Parents
     if (pid >  0)
     {
+		close(channel[0] -> child_to_parent_fd[1]);	// close write-end pipe
+		
         while(1)
-        {
-            read_error = read(channel[0] -> child_to_parent_fd[0], temp, sizeof(temp));
-            if (read_error > 0)
+        {	
+			// Iterate through opened pipe channels to check if there are any commands
+			for (i = 0; i < tab_index; i ++)
             {
-                printf("%d\n", temp -> type);
-                if (temp -> type == 0) 
-                {
-                    if (pipe(router_url) == -1)
-                    {
-                        perror("pipe error");
-                        exit(1);
-                    }
-                    // Set Non block-read from controller.
-                    flags = fcntl(router_url[tab_index][0], F_GETFL, 0);
-                    fcntl(outer_url[tab_index][0], F_SETFL, flags | O_NONBLOCK);
-                    channel[tab_index] -> child_to_parent_fd[0] = router_url[tab_index][0];    
-                    channel[tab_index] -> child_to_parent_fd[1] = router_url[tab_index][1];
-                    // This is not good code for now
-                    channel[tab_index] -> parent_to_child_fd[0] = router_url[tab_index+1][0];    
-                    channel[tab_index] -> parent_to_child_fd[1] = router_url[tab_index+1][1];
-                    int new_tab_pid = fork();
-                    if (new_tab_pid > 0) 
-                    {
-                        
-                    }
-                    else if (new_tab_pid == 0) 
-                    {
-                        url_rendering_process(tab_index, channel[tab_index++]);
-                    }
-                    else
-                    {
-                        perror("Fork Error");
-                    }
-                }
+                if (read(channel[i] -> child_to_parent_fd[0], temp, sizeof(temp)) > 0)
+				{
+					command_type = temp -> type;
+					switch (command_type)
+					{
+						case 0:	// Receive new tab command from controller process
+							channel[tab_index] = (comm_channel *) malloc(sizeof(comm_channel));
+							if (pipe(channel[tab_index] -> child_to_parent_fd) == -1)
+							{
+								perror("pipe error");
+								break;
+							}
+							if (pipe(channel[tab_index] -> parent_to_child_fd) == -1)
+							{
+								perror("pipe error");
+								break;
+							}
+
+							router_create_tab(channel[tab_index], tab_index);
+							tab_index++;
+							break;
+							
+						case 1:	// Receive url command from controller process
+							printf("receivedURL%s\n", temp -> req.uri_req.uri);
+							// write(channel[temp -> req.uri_req.render_in_tab] -> parent_to_child_fd[1], temp -> req.uri_req.uri, sizeof(temp -> req.uri_req.uri));
+							break;
+							
+						case 2: // Tab killed command from url process
+							
+							break;
+							
+						default:
+							break;
+					}
+				}
+
+					
             }
-            usleep(10);
+
+			
+            usleep(1000);
         }            
     }
-    // Child
+    // Child process, controller process
     else if (pid ==  0)
     {     
+		close(channel[0] -> child_to_parent_fd[0]);	// Close read-end of pipe
         controller_process(channel[0]);
     }
     // Fork Error
