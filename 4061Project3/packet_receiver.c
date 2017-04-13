155068dc61a0244677be4b9b132c3a87132ffc30
Tristan Mansfield <mansf043@umn.edu>
2017-04-12 19:16:09 -0500

    DONE

--- 4061Project3/packet_receiver.c
+++ 4061Project3/packet_receiver.c
@@ -1,4 +1,8 @@
-
+/* CSci4061 S2017 Assignment 3
+* login: ohxxx371
+* date: 4/12/2017
+* name: Tristan Mansfield, Marcus Jun Oh
+* id: mansf043, ohxxx371 */
 #include "packet.h"
 
 int msqid = -1;
@@ -7,7 +11,7 @@ static message_t message;   /* current message structure */
 static mm_t mm;             /* memory manager will allocate memory for packets */
 static int pkt_cnt = 0;     /* how many packets have arrived for current message */
 static int pkt_total = 1;   /* how many packets to be received for the message */
-static char *msg;
+static char *msg;           /* character string to hold the assembled message */
 /*
    Handles the incoming packet. 
    Store the packet in a chunk from memory manager.
@@ -15,28 +19,31 @@ static char *msg;
    Hence you need to take care to store and assemble it correctly.
    Example, message "aaabbb" can come as bbb -> aaa, hence, you need to assemble it
    as aaabbb.
-   Hint: "which" field in the packet will be useful.
  */
 static void packet_handler(int sig) {
-  packet_t pkt;
-  void * chunk;
-      
-  // TODO get the "packet_queue_msg" from the queue.
-  packet_queue_msg msg;
-  msgrcv(msqid, (void *) &msg, sizeof(packet_t), QUEUE_MSG_TYPE, 0);
-  // TODO extract the packet from "packet_queue_msg" and store it in the memory from memory manager
-  pkt = msg.pkt;
-  //printf("%d, %d, %s\n", pkt.how_many, pkt.which, pkt.data);
-  chunk = mm_get(&mm);
-  memcpy(chunk, (void *) &pkt, sizeof(pkt));
-  message.data[message.num_packets++] = chunk;
-  pkt_cnt++;
-  pkt_total = pkt.how_many;
+    packet_t pkt;
+    void * chunk;
+    packet_queue_msg msg;
+     
+    // get the "packet_queue_msg" from the queue.
+    msgrcv(msqid, (void *) &msg, sizeof(packet_t), QUEUE_MSG_TYPE, 0);
+    // extract the packet from "packet_queue_msg" and store it in the memory from memory manager
+    pkt = msg.pkt;
+    chunk = mm_get(&mm);
+    memcpy(chunk, (void *) &pkt, sizeof(pkt));
+    message.data[message.num_packets++] = chunk;
+    // update pkt_cnt and pkt_total
+    pkt_cnt++;
+    pkt_total = pkt.how_many;
 }
 
+/*
+   Handles the SIGINT signal. Frees the memory used by the memory manager and closes
+   the message queue before terminating the process.
+ */
 static void sigint_handler(int sig)
 {
-mm_release(&mm);
+    mm_release(&mm);
     free(msg);
     msgctl(msqid, IPC_RMID, 0);
     printf("Receiver Exiting\n");
@@ -44,87 +51,102 @@ mm_release(&mm);
 } 
 
 /*
- * TODO - Create message from packets ... deallocate packets.
- * Return a pointer to the message on success, or NULL
+   Create message from packets.
+   Return a pointer to the message on success.
  */
 static char* assemble_message() {
-  char* msg;
-  int i;
-  int msg_len = message.num_packets * sizeof(data_t);
+    char* msg;
+    int i;
+    packet_t * temp;
+    int msg_len = message.num_packets * sizeof(data_t);
 
-  /* TODO - Allocate msg and assemble packets into it */
-  msg = (char*) malloc(msg_len+1);
-  packet_t * temp;
-  for(i = 0; i < message.num_packets; i++) {
-    temp = (packet_t *) message.data[i];
-    memcpy(msg + ((temp->which) * sizeof(data_t)), (void *) &temp->data, sizeof(data_t));
-  }
-  msg[msg_len] = '\0';  
-  
-  /* reset these for next message */
-  pkt_total = 1;
-  pkt_cnt = 0;
-  message.num_packets = 0;
+    /* Allocate msg and assemble packets into it */
+    msg = (char*) malloc(msg_len+1);
+    for(i = 0; i < message.num_packets; i++) {
+        temp = (packet_t *) message.data[i];
+        memcpy(msg + ((temp->which) * sizeof(data_t)), (void *) &temp->data, sizeof(data_t));
+        //mm_put(&mm, (void *) temp);
+    }
+    msg[msg_len] = '\0';  
+    
+    /* reset these for next message */
+    pkt_total = 1;
+    pkt_cnt = 0;
+    message.num_packets = 0;
 
-  return msg;
+    return msg;
 }
 
+/*
+   The packet_receiver main function initializes the memory manager, message queue,
+   and signal handlers, then runs the loops that keep track of how many messages and
+   packets need to be received. When all of the packets of a message are received, the
+   message is assembled and printed to STDOUT. When all messages have been received,
+   the memory manager's memory is freed and the message queue is closed before the
+   process terminates.
+   */
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    printf("Usage: packet_sender <num of messages to receive>\n");
-    exit(-1);
-  }
-
-  int k = atoi(argv[1]); /* no of messages you will get from the sender */
-  int i;
-
-  /* TODO - init memory manager for NUM_CHUNKS chunks of size CHUNK_SIZE each */
-  mm_init(&mm, NUM_CHUNKS, CHUNK_SIZE);
-
-  message.num_packets = 0;
-
-  /* TODO initialize msqid to send pid and receive messages from the message queue. Use the key in packet.h */
-  msqid = msgget(key, 0666 | IPC_CREAT);
-  
-  /* TODO send process pid to the sender on the queue */
-  int pid = getpid();
-  pid_queue_msg pid_msg;
-  pid_msg.pid = pid;
-  pid_msg.mtype = QUEUE_MSG_TYPE;
-  printf("Sending pid: %d\n", pid);
-  msgsnd(msqid, (void *) &pid_msg, sizeof(int), 0);
-  
-  /* TODO set up SIGIO handler to read incoming packets from the queue. Check packet_handler()*/
-  struct sigaction act, act2;
-  act.sa_handler = packet_handler;
-  sigfillset(&act.sa_mask);
-  sigdelset(&act.sa_mask, SIGINT);
-  sigaction(SIGIO, &act, NULL);
-
-  act2.sa_handler = sigint_handler;
-  sigfillset(&act2.sa_mask);
-  sigaction(SIGINT, &act2, NULL);
-
-  for (i = 1; i <= k; i++) {
-    while (pkt_cnt < pkt_total) {
-      pause(); /* block until next packet */
+    if (argc != 2) {
+        printf("Usage: packet_sender <num of messages to receive>\n");
+        exit(-1);
     }
+
+    int k = atoi(argv[1]); /* no of messages you will get from the sender */
     
-    msg =  assemble_message();
-    if (msg == NULL) {
-      perror("Failed to assemble message");
-    }
-    else {
-      fprintf(stderr, "GOT IT: message=%s\n", msg);
-      free(msg);
-    }
-  }
+    int i;
+    pid_queue_msg pid_msg;
+    int pid;
+    
+    struct sigaction act, act2;
 
-  // TODO deallocate memory manager
-  mm_release(&mm);
+    /* init memory manager for NUM_CHUNKS chunks of size CHUNK_SIZE each */
+    mm_init(&mm, NUM_CHUNKS, CHUNK_SIZE);
 
-  // TODO remove the queue once done
-  msgctl(msqid, IPC_RMID, 0);
-  
-  return EXIT_SUCCESS;
+    message.num_packets = 0;
+
+    /* initialize msqid to send pid and receive messages from the message queue. Use the key in packet.h */
+    msqid = msgget(key, 0666 | IPC_CREAT);
+    
+    /* send process pid to the sender on the queue */
+    pid = getpid();
+    pid_msg.pid = pid;
+    pid_msg.mtype = QUEUE_MSG_TYPE;
+    printf("Sending pid: %d\n", pid);
+    msgsnd(msqid, (void *) &pid_msg, sizeof(int), 0);
+    
+    /* set up SIGIO handler to read incoming packets from the queue. Check packet_handler()*/ 
+    act.sa_handler = packet_handler;
+    sigfillset(&act.sa_mask);
+    sigdelset(&act.sa_mask, SIGINT);
+    sigaction(SIGIO, &act, NULL);
+    
+    /* set up SIGINT handler to terminate the program if the packet_sender encounters an issues*/
+    act2.sa_handler = sigint_handler;
+    sigfillset(&act2.sa_mask);
+    sigaction(SIGINT, &act2, NULL);
+    
+    // receive k messages
+    for (i = 1; i <= k; i++) {
+        // receive pkt_cnt packets
+        while (pkt_cnt < pkt_total) {
+            pause(); /* block until next packet */
+        }
+        
+        msg =  assemble_message();
+        if (msg == NULL) {
+            perror("Failed to assemble message");
+        }
+        else {
+            fprintf(stderr, "GOT IT: message=%s\n", msg);
+            free(msg);
+        }
+    }
+
+    // deallocate memory manager
+    mm_release(&mm);
+
+    // remove the queue once done
+    msgctl(msqid, IPC_RMID, 0);
+    
+    return EXIT_SUCCESS;
 }
--- 4061Project3/packet_sender.c
+++ 4061Project3/packet_sender.c
@@ -1,13 +1,16 @@
-
+/* CSci4061 S2017 Assignment 3
+* login: ohxxx371
+* date: 4/12/2017
+* name: Tristan Mansfield, Marcus Jun Oh
+* id: mansf043, ohxxx371 */
 #include <time.h>
 #include "packet.h"
 
-static int pkt_cnt = 0;     /* how many packets have been sent for current message */
-static int pkt_total = 1;   /* how many packets to send send for the message */
-static int msqid = -1; /* id of the message queue */
-static int receiver_pid; /* pid of the receiver */
-static int terminateFlag = 0;
-
+static int pkt_cnt = 0;       /* how many packets have been sent for current message */
+static int pkt_total = 1;     /* how many packets to send send for the message */
+static int msqid = -1;        /* id of the message queue */
+static int receiver_pid;      /* pid of the receiver */
+static int terminateFlag = 0; /* set to 1 if program needs to terminate */
 
 /*
    Returns the packet for the current message. The packet is selected randomly.
@@ -18,147 +21,165 @@ static int terminateFlag = 0;
    So, message aaabbbccc can be sent as bbb -> aaa -> ccc
    */
 static packet_t get_packet() {
-  static int which = -1;
-  static int how_many;
-  static int num_of_packets_sent = 0;
-  static int is_packet_sent[MAX_PACKETS];
-  int i;
+    static int which = -1;
+    static int how_many;
+    static int num_of_packets_sent = 0;
+    static int is_packet_sent[MAX_PACKETS];
+    int i;
 
-  packet_t pkt;
-  if (num_of_packets_sent == 0) {
-    how_many = rand() % MAX_PACKETS;
-    if (how_many == 0) {
-      how_many = 1;
+    packet_t pkt;
+    if (num_of_packets_sent == 0) {
+        how_many = rand() % MAX_PACKETS;
+        if (how_many == 0) {
+            how_many = 1;
+        }
+        pkt_total = how_many;
+        printf("Number of packets in current message: %d\n", how_many);
+        which = -1;
+        for (i = 0; i < MAX_PACKETS; ++i) {
+            is_packet_sent[i] = 0;
+        }
     }
-    pkt_total = how_many;
-    printf("Number of packets in current message: %d\n", how_many);
-    which = -1;
-    for (i = 0; i < MAX_PACKETS; ++i) {
-      is_packet_sent[i] = 0;
+    which = rand() % how_many;
+    if (is_packet_sent[which] == 1) {
+        i = (which + 1) % how_many;
+        while (i != which) {
+            if (is_packet_sent[i] == 0) {
+                which = i;
+                break;
+            }
+            i = (i + 1) % how_many;
+        } 
     }
-  }
-  which = rand() % how_many;
-  if (is_packet_sent[which] == 1) {
-    i = (which + 1) % how_many;
-    while (i != which) {
-      if (is_packet_sent[i] == 0) {
-        which = i;
-        break;
-      }
-      i = (i + 1) % how_many;
-    } 
-  }
-  pkt.how_many = how_many;
-  pkt.which = which;
+    pkt.how_many = how_many;
+    pkt.which = which;
 
-  memset(pkt.data, 'a' + which, sizeof(data_t));
+    memset(pkt.data, 'a' + which, sizeof(data_t));
 
-  is_packet_sent[which] = 1;
-  num_of_packets_sent++;
-  if (num_of_packets_sent == how_many) {
-    num_of_packets_sent = 0;
-  }
+    is_packet_sent[which] = 1;
+    num_of_packets_sent++;
+    if (num_of_packets_sent == how_many) {
+        num_of_packets_sent = 0;
+    }
 
-  return pkt;
+    return pkt;
 }
 
+/*
+   Handles a SIGINT signal that would cause the program to terminate.
+   Causes a SIGINT signal to be sent to the packet_receiver before termination.
+   */
 static void sigint_handler(int sig){
-  terminateFlag = 1; 
+    terminateFlag = 1; 
 }
 
+/*
+   Handles the SIGALRM signal from the timer that tells the program to send a packet
+   to the packet_receiver process. The contents of the packet are printed to STDOUT,
+   the packet is send via message queue to packet_receiver, and a SIGIO signal
+   is sent to packet_receiver.
+   */
 static void packet_sender(int sig) {
-  //printf ("In SIGALRM handler\n");
-  packet_t pkt;
+    packet_t pkt;
+    packet_queue_msg msg;
 
-  pkt = get_packet();
-  // temp is just used for temporarily printing the packet.
-  char temp[PACKET_SIZE + 2];
-  strcpy(temp, pkt.data);
-  temp[3] = '\0';
-  printf ("Sending packet: %s\n", temp);
-  pkt_cnt++;
+    pkt = get_packet();
+    // temp is just used for temporarily printing the packet.
+    char temp[PACKET_SIZE + 2];
+    strcpy(temp, pkt.data);
+    temp[3] = '\0';
+    printf ("Sending packet: %s\n", temp);
+    pkt_cnt++;
   
-  // TODO Create a packet_queue_msg for the current packet.
-  packet_queue_msg msg;
-  msg.mtype = QUEUE_MSG_TYPE;
-  msg.pkt = pkt;
-  // TODO send this packet_queue_msg to the receiver. Handle any error appropriately.
-  if(msgsnd(msqid, (void *) &msg, sizeof(packet_t), 0) == -1) {
-    perror("Message send failed");
-    terminateFlag = 1;
-    return;
-  }
+    // Create a packet_queue_msg for the current packet.
+    msg.mtype = QUEUE_MSG_TYPE;
+    msg.pkt = pkt;
+    
+    // send this packet_queue_msg to the receiver. Handle any error appropriately.
+    if(msgsnd(msqid, (void *) &msg, sizeof(packet_t), 0) == -1) {
+        perror("Message send failed");
+        terminateFlag = 1;
+        return;
+    }
   
-  // TODO send SIGIO to the receiver if message sending was successful.
-  kill(receiver_pid, SIGIO);
+    // send SIGIO to the receiver if message sending was successful.
+    kill(receiver_pid, SIGIO);
 }
 
+/*
+   The packet_sender main function initializes the message queue, signal handlers,
+   and timer, then runs the loops that keep track of how many messages and packets
+   need to be sent. If the terminate flag is set, the process will send a SIGINT
+   signal to packet_receiver before exiting.
+   */
 int main(int argc, char **argv) {
 
-  if (argc != 2) {
-    printf("Usage: packet_sender <num of messages to send>\n");
-    exit(-1);
-  }
-
-  int k = atoi(argv[1]); /* number of messages  to send */
-  srand(time(NULL)); /* seed for random number generator */
-
-  int i;
-
-  struct itimerval interval;
-  struct sigaction act, act2;           
-
-  /* TODO Create a message queue */ 
-  msqid = msgget(key, 0666 | IPC_CREAT);
-  
-  /* TODO read the receiver pid from the queue and store it for future use*/
-  printf("Waiting for receiver pid.\n");
-  pid_queue_msg msg;
-  msgrcv(msqid, (void *) &msg, sizeof (int), QUEUE_MSG_TYPE, 0);
-  receiver_pid = msg.pid;
-  printf("Got pid : %d\n", receiver_pid);
- 
-  /* TODO - set up alarm handler -- mask all signals within it */
-  /* The alarm handler will get the packet and send the packet to the receiver. Check packet_sender();
-   * Don't care about the old mask, and SIGALRM will be blocked for us anyway,
-   * but we want to make sure act is properly initialized.
-   */
-  act.sa_handler = packet_sender;
-  sigfillset(&act.sa_mask);
-  sigdelset(&act.sa_mask, SIGINT);
-  sigaction(SIGALRM, &act, NULL);
-  
-  act2.sa_handler = sigint_handler;
-  sigfillset(&act2.sa_mask);
-  sigaction(SIGINT, &act2, NULL);
-  /*  
-   * TODO - turn on alarm timer ...
-   * use  INTERVAL and INTERVAL_USEC for sec and usec values
-  */
-  interval.it_value.tv_sec = INTERVAL;
-  interval.it_value.tv_usec = INTERVAL_USEC;
-  setitimer(ITIMER_REAL, &interval, NULL);
-  /* And the timer */
-
-  /* NOTE: the below code wont run now as you have not set the SIGALARM handler. Hence, 
-     set up the SIGALARM handler and the timer first. */
-  for (i = 1; i <= k; i++) {
-    printf("==========================%d\n", i);
-    printf("Sending Message: %d\n", i);
-  /* And the timer */
-    while (pkt_cnt < pkt_total) {
-      pause(); /* block until next packet is sent. SIGALARM will unblock and call the handler.*/
-      if (terminateFlag)
-      {
-        kill(receiver_pid, SIGINT);
-        printf("Sender Exiting\n");
-        exit(0); 
-      }
-      setitimer(ITIMER_REAL, &interval, NULL);
+    if (argc != 2) {
+        printf("Usage: packet_sender <num of messages to send>\n");
+        exit(-1);
     }
-    //usleep(5000000);
-    pkt_cnt = 0;
-  }
 
-  return EXIT_SUCCESS;
+    int k = atoi(argv[1]); /* number of messages  to send */
+    srand(time(NULL)); /* seed for random number generator */
+
+    int i;
+    pid_queue_msg msg;
+
+    struct itimerval interval;
+    struct sigaction act, act2;           
+
+    /* Create a message queue */ 
+    msqid = msgget(key, 0666 | IPC_CREAT);
+  
+    /* read the receiver pid from the queue and store it for future use*/
+    printf("Waiting for receiver pid.\n");
+    
+    msgrcv(msqid, (void *) &msg, sizeof (int), QUEUE_MSG_TYPE, 0);
+    receiver_pid = msg.pid;
+    printf("Got pid : %d\n", receiver_pid);
+   
+    /* set up alarm handler -- mask all signals within it */
+    /* The alarm handler will get the packet and send the packet to the receiver. Check packet_sender();
+     * Don't care about the old mask, and SIGALRM will be blocked for us anyway,
+     * but we want to make sure act is properly initialized.
+     */
+    act.sa_handler = packet_sender;
+    sigfillset(&act.sa_mask);
+    sigdelset(&act.sa_mask, SIGINT);
+    sigaction(SIGALRM, &act, NULL);
+    
+    // set up handler for SIGINT
+    act2.sa_handler = sigint_handler;
+    sigfillset(&act2.sa_mask);
+    sigaction(SIGINT, &act2, NULL);
+    /*  
+     * turn on alarm timer ...
+     * use  INTERVAL and INTERVAL_USEC for sec and usec values
+    */
+    interval.it_value.tv_sec = INTERVAL;
+    interval.it_value.tv_usec = INTERVAL_USEC;
+    interval.it_interval.tv_sec = 0;
+    interval.it_interval.tv_usec = 0;
+    setitimer(ITIMER_REAL, &interval, NULL);
+
+    /* send k messages */
+    for (i = 1; i <= k; i++) {
+        printf("==========================%d\n", i);
+        printf("Sending Message: %d\n", i);
+        /* send packet pkt_total packets */
+        while (pkt_cnt < pkt_total) {
+            pause(); /* block until next packet is sent. SIGALARM will unblock and call the handler.*/
+            // if SIGINT is received, exit program and send SIGINT to receiver process
+            if (terminateFlag)
+            {
+                kill(receiver_pid, SIGINT);
+                printf("Sender Exiting\n");
+                exit(0); 
+            }
+            setitimer(ITIMER_REAL, &interval, NULL);
+        }
+        pkt_cnt = 0;
+    }
+
+    return EXIT_SUCCESS;
 }
