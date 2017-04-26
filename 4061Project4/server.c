24a60bc1683615ea0b310449904e39900c24bd65
Tristan Mansfield <mansf043@umn.edu>
2017-04-25 19:10:22 -0500

    Fixed server

--- 4061Project4/server.c
+++ 4061Project4/server.c
@@ -17,33 +17,65 @@
 #define MAX_THREADS 100
 #define MAX_QUEUE_SIZE 100
 #define MAX_REQUEST_LENGTH 1024
-
 //Structure for a single request.
 typedef struct request
 {
-        int             m_socket;
-        char    m_szRequest[MAX_REQUEST_LENGTH];
+    int     m_socket;
+    char    m_szRequest[MAX_REQUEST_LENGTH];
 } request_t;
+
+// Mutex and ring buffer declaration
+pthread_mutex_t request_access = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t slot_availble = PTHREAD_COND_INITIALIZER;
+pthread_cond_t request_availble = PTHREAD_COND_INITIALIZER;
+int dispatcher_count;
+int worker_count;
+int numb_dispatcher, numb_worker;
 
 void * dispatch(void * arg)
 {
-        return NULL;
+    while(1)
+    {
+        pthread_mutex_lock(&request_access);
+        while (dispatcher_count == numb_dispatcher)
+            pthread_cond_wait(&slot_availble, &request_access);
+        
+        
+    }
+    return NULL;
 }
 
 void * worker(void * arg)
 {
-        return NULL;
+    return NULL;
 }
 
 int main(int argc, char **argv)
 {
-        //Error check first.
-        if(argc != 6 && argc != 7)
-        {
-                printf("usage: %s port path num_dispatcher num_workers queue_length [cache_size]\n", argv[0]);
-                return -1;
-        }
+    int port;
+    int i;
+    pthread_t dispatcher_threads[numb_dispatcher];
+    pthread_t worker_threads[numb_worker];
 
-        printf("Call init() first and make a dispather and worker threads\n");
-        return 0;
+    //Error check first.
+    if(argc != 6 && argc != 7)
+    {
+        printf("usage: %s port path num_dispatcher num_workers queue_length [cache_size]\n", argv[0]);
+        return -1;
+    }
+    // Parse the arguments when there is no error to the arguments.
+    port = atoi(argv[1]);
+    numb_dispatcher = atoi(argv[3]);
+    numb_worker = atoi(argv[4]);
+    
+    init(port);
+    for(i = 0; i < numb_dispatcher; i ++) // create dispather threads;
+        pthread_create(&dispatcher_threads[i], NULL, dispatch, NULL);
+    for(i = 0; i < numb_worker; i ++)
+        pthread_create(&worker_threads[i], NULL, worker, NULL);
+
+
+    pause();
+    printf("Call init() first and make a dispather and worker threads\n");
+    return 0;
 }
